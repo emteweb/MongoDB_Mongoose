9921f8f38989fadd35ffe93485a9cd92213e2719
Marcin T <fake_mail@gmail.com>
2021-01-24 13:10:00 +0100

    Filtering by category

--- Products_CRUD/index.js
+++ Products_CRUD/index.js
@@ -27,9 +27,14 @@ app.use(methodOverride('_method'));
 const categories = ['fruit', 'vegetable', 'dairy', 'fungi'];
 
 app.get('/products', async (req,res) => {
-    const products = await Product.find({});
-    console.log(products);
-    res.render('products/index', {products})
+    const {category} = req.query;
+    if (category) {
+        const products = await Product.find({category});
+        res.render('products/index', {products, category})
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', {products, category: 'All'})
+    }
 })
 
 // template for adding a new product
